642725c18e9ad9f3d0acd630bf6f5f264eb70061
Yasmeen-aboeleneen <yasmeenaboeleneen3@gmail.com>
2024-09-01 06:22:22 +0300

    Scan Barcode

--- lib/Views/Home/Mobile/QRCode/scan_barcode.dart
+++ lib/Views/Home/Mobile/QRCode/scan_barcode.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:scan_app/Core/Constants/colors.dart';
 
 class ScanBarcode extends StatelessWidget {
   const ScanBarcode({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: kveryWhite,
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        children: [],
+      ),
+    );
   }
 }
